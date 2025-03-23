308d7b40a2fdc6378297baf483463b2952acdba3
Ahmet Resul Kılınç <ahmet.resul.kilinc@hotmail.com>
2025-03-23 21:51:14 +0300

    Everything has been done.

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import turtle
 import random
 import time
 
-#Screen
+# Screen setup
 screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catch The Turtle")
 
 FONT = ('Arial', 30, 'normal')
 
+game_over = False
 
-#Score Turtle
 
+# Countdown Turtle
+countdown_turtle = turtle.Turtle()
+
+# Score Turtle setup
 score_turtle = turtle.Turtle()
+score = 0
 
 def setup_score_turtle():
     score_turtle.hideturtle()
@@ -26,42 +31,71 @@ def setup_score_turtle():
     score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
 
+
+
+
 def make_turtle():
     kaplumbaga = turtle.Turtle()
-    kaplumbaga.hideturtle()  # Başlangıçta kaplumbağa görünmesin
-    kaplumbaga.shape("turtle")
-    kaplumbaga.speed(1)
-    kaplumbaga.shapesize(2, 2)
 
-    degree = [30, 60, 90, 45, 71, 16, -30, -45, -60, -90, -270, -300]
-    forward = [50, 90, 150, 200, 300, 250, 125]
+    if not game_over:
+
+        def click_turtle(x, y):
+            global score
+            score += 1
+            score_turtle.clear()
+            score_turtle.write(arg=f"Score: {score}", move=False, align="center", font=FONT)
+
+            print(f"Kaplumbağa tıklandı: x = {x}, y = {y}")
 
 
-    start_position = kaplumbaga.position()
-
-    i = 1
-    while i <= 20:
-        kaplumbaga.hideturtle()
-
+        kaplumbaga.shape("turtle")
+        kaplumbaga.speed(1)
+        kaplumbaga.shapesize(2, 2)
         kaplumbaga.penup()
 
-        kaplumbaga.goto(start_position)
+        degree = [30, 60, 90, 45, 71, 16, -30, -45, -60, -90, -270, -300, -238]
+        forward = [50, 90, 150, 200, 300, 250, 125, 10]
 
-        selected_degree = random.choice(degree)
-        selected_forward = random.choice(forward)
-
-        kaplumbaga.left(selected_degree)
-        kaplumbaga.forward(selected_forward)
-
-        kaplumbaga.hideturtle()
+        kaplumbaga.goto(random.randint(-200, 200), random.randint(-200, 200))
         kaplumbaga.showturtle()
-        time.sleep(1.5)
-        i += 1
+
+        kaplumbaga.onclick(click_turtle)
+
+
+        for i in range(20):
+            kaplumbaga.hideturtle()
+            kaplumbaga.penup()
+            kaplumbaga.goto(random.randint(-200, 200), random.randint(-200, 200))
+            kaplumbaga.showturtle()
+            time.sleep(1)
+            kaplumbaga.onclick(click_turtle)
+
+def countdown(time):
+    global game_over
+    countdown_turtle.hideturtle()
+    countdown_turtle.penup()
+
+    top_height = screen.window_height() / 2
+    y = top_height * 0.9
+
+    countdown_turtle.setposition(0, y - 40)
+    countdown_turtle.clear()
+    if time  > 0:
+        countdown_turtle.clear()
+        countdown_turtle.write(arg="Time: {}".format(time), move=False, align="center", font=FONT)
+        screen.ontimer(lambda: countdown(time - 1), 1000 )
+
+    else:
+        game_over = True
+        countdown_turtle.clear()
+        countdown_turtle.write(arg="Game Over!", move=False, align="center", font=FONT)
 
 
 
 
 setup_score_turtle()
+countdown(10)
 make_turtle()
-turtle.mainloop()
 
+
+turtle.mainloop()
